0436897a20998118bb6a0c00222a3cec9ab81561
anoushkaakhoury <anoushkaakhoury@gmail.com>
2021-03-04 10:45:23 +0530

    stort

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 cd /home/ubuntu
 #npm --prefix /home/ubuntu/app.js start
-sudo pm2 restart app.js
+#sudo pm2 restart app.js
+sudo pm2 stop /home/ubuntu/app.js
+sudo pm2 start /home/ubuntu/app.js
 #npm  --prefix /home/ubuntu/app.js start
+
+
+
